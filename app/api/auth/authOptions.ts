3826950ca27cf3d797d8c694b091ebe0b3a7ef4a
Edjery Gumbao <edisgumbao@gmail.com>
2023-12-20 09:41:42 +0800

    fixed authOption error

--- app/api/auth/authOptions.ts
+++ app/api/auth/authOptions.ts
@@ -1,9 +1,9 @@
+import prisma from "@/prisma/client";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/prisma/client";
 import bcrypt from "bcrypt";
-import { NextAuthOptions } from "next-auth";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -11,7 +11,11 @@ export const authOptions: NextAuthOptions = {
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        email: { label: "Email", type: "email", placeholder: "Email" },
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "Email",
+        },
         password: {
           label: "Password",
           type: "password",
@@ -20,19 +24,23 @@ export const authOptions: NextAuthOptions = {
       },
       async authorize(credentials, req) {
         if (!credentials?.email || !credentials.password) return null;
+
         const user = await prisma.user.findUnique({
           where: { email: credentials.email },
         });
+
         if (!user) return null;
+
         const passwordsMatch = await bcrypt.compare(
           credentials.password,
           user.hashedPassword!
         );
+
         return passwordsMatch ? user : null;
       },
     }),
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLiENT_ID!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
